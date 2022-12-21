bc638f0afb6c296b2c83d483c27f501ec7dcd701
Carlosr08 <crivascyt@gmail.com>
2022-12-21 00:48:27 -0400

    base de datos y autenticacion

--- start/routes.ts
+++ start/routes.ts
@@ -1,25 +1,40 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
+import User from 'App/Models/User'
 
 Route.get('/', async () => {
   return { hello: 'world' }
+})
+
+Route.post('login', async ({ auth, request, response }) => {
+  const email = request.input('email')
+  const password = request.input('password')
+
+  try {
+    const token = await auth.use('api').attempt(email, password)
+    return token
+  } catch {
+    return response.unauthorized('Invalid credentials')
+  }
+})
+
+Route.post('/logout', async ({ auth }) => {
+  await auth.use('api').revoke()
+  return {
+    revoked: true
+  }
+})
+
+Route.get('/google', async ({ ally, auth }) => {
+  return ally.use('google').redirect()
+  // const google = ally.use('google')
+  // const googleUser = await google.user()
+  // const user = await User.firstOrCreate({
+  //   email: googleUser.email,
+  // }, {
+  //   name: googleUser.name,
+  //   accessToken: googleUser.token.token,
+  //   isVerified: googleUser.emailVerificationState === 'verified'
+  // })
+  // await auth.use('api').login(user)
+  // return google.user()
 })
